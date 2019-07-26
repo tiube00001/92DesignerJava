335b1202a8d220a36eb86b827f7b1242ceecd11b
Hailong Yang <408338433@qq.com>
2019-07-26 10:07:16 +0800

    yhl

--- src/main/java/com/designer_two/mp/mapper/AdminUsersMapper.java
+++ src/main/java/com/designer_two/mp/mapper/AdminUsersMapper.java
@@ -12,5 +12,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2019-07-25
  */
 public interface AdminUsersMapper extends BaseMapper<AdminUsers> {
-
+    AdminUsers oneUser(int id);
 }
--- src/test/java/com/designer_two/DesignerTwoApplicationTests.java
+++ src/test/java/com/designer_two/DesignerTwoApplicationTests.java
@@ -50,5 +50,10 @@ public class DesignerTwoApplicationTests {
         System.out.println(adminUsersMapper.selectById(1));
     }
 
+    @Test
+    public void testXmlMap() {
+        System.out.println(adminUsersMapper.oneUser(1));
+    }
+
 }
 
